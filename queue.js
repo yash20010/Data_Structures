8538cd98942224758b423a02077a1ff61073f919
throne.yash@gmail.com <throne.yash@gmail.com>
2021-08-12 10:12:55 -0600

    Adding queue data structure

---
+++ queue.js
@@ -0,0 +1,18 @@
+// Queue follows First in First out protocol (FIFO)
+
+module.exports = class Queue {
+  constructor(...items) {
+    this.reverse = false
+    this.queue = [...items]
+  }
+
+  enqueue(...items) {
+    return this.reverse
+      ? this.queue.push(...items)
+      : this.queue.unshift(...items)
+  }
+
+  dequeue() {
+    return this.reverse ? this.queue.shift() : this.queue.pop()
+  }
+}
